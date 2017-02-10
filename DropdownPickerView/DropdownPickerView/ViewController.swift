08a933ca19575054369e2f03bf384225a2d848f6
Aatish Rajkarnikar <aatish.rajkarnikar@gmail.com>
2017-02-10 15:02:40 +0545

    Confirmed to picker view delegate

--- DropdownPickerView/DropdownPickerView/ViewController.swift
+++ DropdownPickerView/DropdownPickerView/ViewController.swift
@@ -8,19 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,DropdownPickerViewDelegate {
 
     @IBOutlet var pickerView: DropdownPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let countries = NSLocale.isoCountryCodes.map { (code:String) -> String in
             let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: code])
             return NSLocale(localeIdentifier: "en_US").displayName(forKey: NSLocale.Key.identifier, value: id) ?? "Country not found for code: \(code)"
         }
         pickerView.dataSource = countries
+        pickerView.delegate = self
+    }
+    
+    func pickerView(pickerView: DropdownPickerView, didSelectAt index: NSInteger, withValue value: String) {
+        //Is called when user select an option from dropdown.
     }
 
-
 }
+
 
